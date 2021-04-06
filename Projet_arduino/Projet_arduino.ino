a2e060b75fdc8429e4c6b1607130068d4a1add0e
NicolasPiques <npiques87500@gmail.com>
2021-04-06 13:31:08 +0200

    Init projet arduino

---
+++ Projet_arduino/Projet_arduino.ino
@@ -0,0 +1,13 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED_BUILTIN,OUTPUT);
+  digitalWrite(LED_BUILTIN,1);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+
+
+}
